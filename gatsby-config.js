8cdbfeef41df73b02b37ab661d70db661d71ece4
Jackson Arceno <39195235+JacksonFA@users.noreply.github.com>
2020-08-19 14:30:51 -0300

    Cleanup starter stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Udemy Gatsby Course`,
+    description: `A blog developed to learn about Gatsby`,
+    author: `@JacksonFA`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
